f171ff7d56d84154063590cc9912ec86dc600414
tatianau04 <tatianau.pcnet@gmail.com>
2026-06-14 23:20:46 +0000

    Actividad semana 6

--- Semana6/server.js
+++ Semana6/server.js
@@ -16,19 +16,6 @@ app.post('/registro', (req, res) => {
 
 });
 
-app.post('/incidencia', (req, res) => {
-
-  const tipo = req.body.tipo;
-  const descripcion = req.body.descripcion;
-
-  res.json({
-    mensaje: "Incidencia registrada",
-    tipo: tipo,
-    descripcion: descripcion
-  });
-
-});
-
 app.listen(3000, () => {
   console.log('Servidor ejecutándose en puerto 3000');
 });
