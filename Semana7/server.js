f0d9d7fdb767b80d2f698abc4aa362a2f0607853
tatianau04 <tatianau.pcnet@gmail.com>
2026-06-14 23:55:30 +0000

    Actividad semana 7

---
+++ Semana7/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let reportes = [];
+
+app.get('/', (req, res) => {
+res.send('Servidor funcionando correctamente');
+});
+
+app.get('/reportes', (req, res) => {
+res.json(reportes);
+});
+
+app.post('/reportes', (req, res) => {
+
+const reporte = {
+id: reportes.length + 1,
+tipo: req.body.tipo,
+descripcion: req.body.descripcion
+};
+
+reportes.push(reporte);
+
+res.json({
+mensaje: "Reporte registrado",
+reporte: reporte
+});
+
+});
+
+app.listen(3000, () => {
+console.log('Servidor ejecutándose en puerto 3000');
+});
